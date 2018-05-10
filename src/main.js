b5029bce4b9f8c560d1b9c496915ce6f5687158a
Umpoy <iumpoy@yahoo.com>
2018-05-10 07:39:12 -0700

    basic game functionality add

--- src/main.js
+++ src/main.js
@@ -4,17 +4,27 @@ $(document).ready(initialize());
 
 function initialize() {
     assignClickHandlers();
+    console.log(hiddenNum)
 }
 
 function assignClickHandlers() {
     $('.reset').on('click', function () {
         //needs to reset game
     });
-    $('.guess').on('click', function () {
-        // submit guess
-    })
+    $('.guess').on('click', guessSubmited);
 }
 
 function getRandomNum() {
     return Math.ceil(Math.random() * 100);
+}
+
+function guessSubmited() {
+    console.log($('input').val());
+    if ($('input').val() > hiddenNum) {
+        $('.displayText').text('to high')
+    } else if ($('input').val() < hiddenNum) {
+        $('.displayText').text('to low')
+    } else {
+        $('.displayText').text('you guess it')
+    }
 }
